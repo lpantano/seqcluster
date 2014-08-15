eb9e9b766aa5f470a7909e13820d95b7c64db768
Lorena <lorena.pantano@gmail.com>
2014-08-15 19:28:47 -0400

    add new reqs

--- setup.py
+++ setup.py
@@ -4,15 +4,19 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+with open("requirements.txt", "r") as f:
+        install_requires = [x.strip() for x in f.readlines() if not x.startswith("#")]
+
+
 setup(name='seqcluster',
-      version='0.99',
+      version='0.99.1',
       description='Small RNA-seq analysis',
       long_description=readme(),
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
       ],
       keywords='RNA-seq miRNA snoRNA NGS',
       url='http://github.com/lpantano/seqcluster',
@@ -20,6 +24,7 @@ setup(name='seqcluster',
       author_email='lpantano@iscb.org',
       license='MIT',
       packages=['seqcluster'],
+      install_requires=install_requires,
       test_suite='nose',
       entry_points={
           'console_scripts': ['seqcluster=seqcluster.command_line:main'],
