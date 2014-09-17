3a15cd0f3c8ac29114c85dff493e98ca2c96fdf6
Lorena <lorena.pantano@gmail.com>
2014-09-16 20:32:11 -0400

    some more empty functions for explore tool

--- seqcluster/libs/read.py
+++ seqcluster/libs/read.py
@@ -1,11 +1,32 @@
-def get_sequences_from_cluster():
-	return TRUE
+"""functions for explore tool"""
 
-def get_precursors_from_cluster():
-	return TRUE
 
-def map_to_precursors():
-	return TRUE
+def load_data(in_file):
+    """load json file from seqcluster cluster"""
+    with open(in_file) as in_handle:
+        return json.load(in_handle)
 
-def plot_positions()
-	return TRUE
+
+def get_sequences_from_cluster(c1, c2, data):
+    """get all sequences from on cluster"""
+    seqs1 = data[c1]['seqs']
+    seqs2 = data[c2]['seqs']
+    return seqs1 + seqs2
+
+
+def get_precursors_from_cluster(c1, c2, data):
+    loci1 = data[c1]['loci']
+    loci2 = data[c2]['loci']
+    return loci1 + loci2
+
+
+def map_to_precursors(seqs, loci):
+	return True
+
+
+def get_fasta(loci):
+    return True
+
+
+def plot_positions():
+	return True
