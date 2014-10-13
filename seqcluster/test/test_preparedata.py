8298a4fe43dc6a0e2018cacf9310cbe480a2277b
Lorena <lorena.pantano@gmail.com>
2014-10-13 13:06:37 -0400

    adapt test to new args

--- seqcluster/test/test_preparedata.py
+++ seqcluster/test/test_preparedata.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from collections import namedtuple
 import os 
 import inspect
 from seqcluster.preparedata import _read_fasta_files,_create_matrix_uniq_seq
@@ -6,13 +7,15 @@ import seqcluster
 
 class TestPreparedata(TestCase):
     def test_preparedata(self):
-    	mod_dir = os.path.dirname(inspect.getfile(seqcluster)).replace("seqcluster/","")
-    	os.chdir(os.path.join(mod_dir,"data"))
-    	if os.path.exists("pre/seqs.ma"):
+        mod_dir = os.path.dirname(inspect.getfile(seqcluster)).replace("seqcluster/","")
+        os.chdir(os.path.join(mod_dir,"data"))
+        if os.path.exists("pre/seqs.ma"):
             os.remove("pre/seqs.ma")
         if os.path.exists("pre/seqs.fa"):
-    	    os.remove("pre/seqs.fa")
-        seq_l, list_s = _read_fasta_files(f=open("config"))
+            os.remove("pre/seqs.fa")
+        arg = namedtuple('args', 'minl maxl minc')
+        args = arg(15, 40, 1)
+        seq_l, list_s = _read_fasta_files(open("config"), args)
         ma_out = open("pre/seqs.ma",'w')
         seq_out = open("pre/seqs.fa",'w')
         _create_matrix_uniq_seq(list_s,seq_l,ma_out,seq_out)
