87704c09590fb386da884bfa3f5eb0da6f976759
Lorena <lorena.pantano@gmail.com>
2014-10-14 14:48:59 -0400

    convert to int input values/2

--- seqcluster/preparedata.py
+++ seqcluster/preparedata.py
@@ -63,7 +63,7 @@ def _read_fasta_files(f, args):
                     counts = re.search("x([0-9]+)", line.strip()).group(1)
                 else:
                     seq = line.strip()
-                    seq = seq[0:args.maxl] if len(seq) > int(args.maxl) else seq
+                    seq = seq[0:int(args.maxl)] if len(seq) > int(args.maxl) else seq
                     if counts > int(args.minc) and len(seq) > int(args.minl):
                         if seq not in seq_l:
                             seq_l[seq] = sequence_unique(idx, seq)
