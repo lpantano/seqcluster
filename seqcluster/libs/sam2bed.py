30c2fa06ff4e62e40e872063af35250407b49019
Lorena <lorena.pantano@gmail.com>
2014-10-20 11:29:50 -0400

    remove position subs

--- seqcluster/libs/sam2bed.py
+++ seqcluster/libs/sam2bed.py
@@ -14,44 +14,33 @@ from classes import bedaligned
 def processSAM(line):
         """
                 Load a SAM file and convert each line to BED format.
-        """            
-#       f = open(file,'r')
-#       for line in f.readlines():
+        """
         samLine = splitLine(line.strip())
         return makeBED(samLine)
-#       f.close()      
-       
-                                       
+
+
 def makeBED(samFields):
-       
         samFlag = int(samFields.flag)
-       
         # Only create a BED entry if the read was aligned
         if (not (samFlag & 0x0004)):
-               
                 chrom = samFields.rname
-                start = str(int(samFields.pos)-1)
+                start = str(int(samFields.pos))
                 end = str(int(samFields.pos) + len(samFields.seq) - 1)
-                name = samFields.qname    
+                name = samFields.qname
                 strand = getStrand(samFlag)
-
-                # Let's use the edit distance as the BED score.
-                # Clearly alternatives exist.
-                #editPattern = re.compile('NM\:i\:(\d+)')
-                #editDistance = editPattern.findall(samFields[12])
-
                 # Write out the BED entry
                 return bedaligned("%s\t%s\t%s\t%s\t.\t%s\n" %  (chrom,start,end,name,strand))
         else:
-                return False    
-               
+                return False
+
+
 def splitLine(line, delim="\t"):
         splitline = line.split(delim)
-        return splitline                
+        return splitline
 
 
 def getStrand(samFlag):
         strand = "+"
         if (samFlag & (0x10)):  # minus strand if true.
-                strand = "-"            
+                strand = "-"
         return strand
