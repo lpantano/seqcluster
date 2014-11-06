a7b1d3212f23fb5739a6c5d6359b51ea55a23173
Lorena <lorena.pantano@gmail.com>
2014-11-06 10:03:28 -0500

    get counts per DB as well

--- seqcluster/stats.py
+++ seqcluster/stats.py
@@ -16,8 +16,8 @@ def stats(args):
     data = parse_ma_file(args.ma)
     logger.info("Get sequences from sam")
     is_align = _read_sam(args.sam)
-    is_json = _read_json(args.json)
-    res = _summarise_sam(data, is_align, is_json)
+    is_json, is_db = _read_json(args.json)
+    res = _summarise_sam(data, is_align, is_json, is_db)
     _write_suma(res, os.path.join(args.out, "stats_align.dat"))
     logger.info("Done")
 
@@ -35,24 +35,31 @@ def _read_sam(sam):
 def _read_json(fn_json):
     """read json information"""
     is_json = set()
+    is_db = {}
     with open(fn_json) as handle:
         data = json.load(handle)
         for item in data[0].values():
             seqs_name = map(lambda (x): x.keys(), item['seqs'])
+            db_name = item['valid'] if "valid" in item else None
             [is_json.add(name[0]) for name in seqs_name]
-    return is_json
+            if db_name:
+                [is_db.update({name[0]: ",".join(db_name)}) for name in seqs_name]
+    return is_json, is_db
 
 
-def _summarise_sam(counts, is_align, is_json):
+def _summarise_sam(counts, is_align, is_json, is_db):
     summary_align = defaultdict(Counter)
     summary_json = defaultdict(Counter)
+    summary_db = defaultdict(Counter)
     for s, o in counts.iteritems():
         l = len(o.seq)
         if s in is_align:
             summary_align[l].update(o.freq)
         if s in is_json:
             summary_json[l].update(o.freq)
-    return [summary_align, summary_json]
+        if s in is_db:
+            summary_db[(l, is_db[s])].update(o.freq)
+    return [summary_align, summary_json, summary_db]
 
 
 def _write_suma(d, fn):
@@ -63,3 +70,6 @@ def _write_suma(d, fn):
         for l, counts in d[1].iteritems():
             for e, freq in counts.iteritems():
                 handle.write("%s %s %s json\n" % (l, e, freq))
+        for l, counts in d[2].iteritems():
+            for e, freq in counts.iteritems():
+                handle.write("%s %s %s %s\n" % (l[0], e, freq, l[1]))
