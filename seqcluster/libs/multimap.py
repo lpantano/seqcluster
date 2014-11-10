7c5f4743911269199916849132955fe314b5ed47
Lorena <lorena.pantano@gmail.com>
2014-11-09 21:22:28 -0500

    inint peak detection and bayesian method for multimapped events

---
+++ seqcluster/libs/multimap.py
@@ -0,0 +1,19 @@
+"""assign a proportion to sequences that map to multiple places"""
+
+def _likelihood():
+    """get likelihood to be from a or b"""
+    return True
+
+
+#get class for bayesian method
+
+
+def _get_sequences_around():
+    """return number of sequences around +/- 5 nt"""
+    return True
+
+
+def solve():
+    #call _get_sequences_around
+    #call likelihood
+    return True
---
+++ seqcluster/libs/peaks.py
@@ -0,0 +1,24 @@
+"""detect peaks inside clusters"""
+
+
+def _scan(positions):
+    """get the region inside the vector with more expression"""
+    scores = []
+    for start in range(0, len(positions) - 17, 5):
+        end = start = 17
+        scores.add(_enrichment(positions[start:end], positions[:start], positions[end:]))
+    #get index max score (check ties)
+    #return enriched region(give up to two region)
+
+
+def _enrichment(resgion, flank_a, flank_b):
+    return True
+
+def _get_locus(cluster):
+    """get the bigger locus"""
+    return True
+
+
+def _get_position_in_loci(locus_id, seqs, loci):
+    """get position in locus of all sequences"""
+    return True
