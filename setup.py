14bf42dbb68bb03fc423f9b9a5fd037e1f4cb2fc
Lorena <lorena.pantano@gmail.com>
2015-01-19 16:54:37 -0500

    update release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='seqcluster',
-      version='0.99.4',
+      version='0.99.5',
       description='Small RNA-seq analysis',
       long_description=readme(),
       classifiers=[
