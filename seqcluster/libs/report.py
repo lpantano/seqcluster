ea4e9e5404a2b18d2d790264584376d84d80340b
Lorena <lorena.pantano@gmail.com>
2015-02-09 19:08:17 -0500

    create figures

--- seqcluster/libs/report.py
+++ seqcluster/libs/report.py
@@ -6,6 +6,7 @@ import matplotlib
 matplotlib.use('Agg')
 import pylab
 pylab.rcParams['figure.figsize'] = (35.0, 12.0)
+from collections import Counter
 import pandas as pd
 
 from read import map_to_precursors
@@ -25,14 +26,25 @@ def make_profile(data, out_dir, args):
         _single_cluster(c, data, os.path.join(out_dir, c, "maps.tsv"), args)
 
 
+def _expand(dat, counts, start, end):
+    """
+    expand the same counts from start to end
+    """
+    for pos in range(start, end):
+        dat[pos] += counts
+    return dat
+
+
 def _convert_to_df(in_file):
-    dat = {}
+    dat = Counter()
     with open(in_file) as in_handle:
         for line in in_handle:
             cols = line.strip().split(" ")
-            counts = int(cols[1].replace("cx", ""))
-            dat[4] = counts
-    df = pd.data_frame(dat)
+            counts = float(cols[1].replace("cx", ""))
+            dat = _expand(dat, counts, int(cols[4]), int(cols[5]))
+    dat = {'positions': dat.keys(), 'expression': dat.values()}
+    df = pd.DataFrame(data=dat, columns=dat.keys())
+    df.set_index('positions', inplace=True)
     return df
 
 
@@ -40,7 +52,7 @@ def _single_cluster(c, data, out_file, args):
     # dat_freq = Counter()
     # for s in data[0][c]['freq']:
     #     dat_freq[s.keys()[0]] = round(sum(s.values()[0].values()))
-
+    figure_file = out_file.replace(".tsv", ".png")
     names = [round(sum(s.values()[0].values())) for s in data[0][c]['freq']]
     seqs = [s.values()[0] for s in data[0][c]['seqs']]
 
@@ -55,5 +67,6 @@ def _single_cluster(c, data, out_file, args):
 
     logger.info("plot sequences on loci")
     df = _convert_to_df(out_file)
-    plot = graph.plot()
-    plot.set_ylabel('% CPU')
+    plot = df.plot()
+    plot.set_ylabel('Normalized expression')
+    plot.get_figure().savefig(figure_file)
