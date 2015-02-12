8efd75374d26bbc453afe90f31fbb465e08d6cb3
Lorena <lorena.pantano@gmail.com>
2015-02-12 18:13:00 -0500

    add cluster to nosetest

---
+++ seqcluster/test/test_cluster.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from collections import namedtuple
+import os
+import inspect
+import pybedtools
+import seqcluster.libs.parameters
+import seqcluster.libs.logger as mylog
+
+from seqcluster.libs.tool import reduceloci
+from seqcluster.libs.cluster import detect_clusters
+from seqcluster.libs.logger import initialize_logger
+from seqcluster.libs.tool import parse_ma_file
+
+
+class TestCluster(TestCase):
+    def test_cluster(self):
+        mod_dir = os.path.dirname(inspect.getfile(seqcluster)).replace("seqcluster/", "")
+        os.chdir(os.path.join(mod_dir, "data/examples"))
+        arg = namedtuple('args', 'debug print_debug MIN_SEQ')
+        args = arg(True, True, 1)
+        seqL = parse_ma_file("seqs_set.ma")
+        c = pybedtools.BedTool("2_clusters_2_seqs_shared")
+        initialize_logger(".", args.debug, args.print_debug)
+        logger = mylog.getLogger(__name__)
+        clus_obj = detect_clusters(c, seqL, args.MIN_SEQ)
+        clusLred = reduceloci(clus_obj, {}, ".")
+        # self.assertTrue(os.path.exists("pre/seqs.fa"))
--- seqcluster/test/test_preparedata.py
+++ seqcluster/test/test_preparedata.py
@@ -1,24 +1,24 @@
 from unittest import TestCase
 from collections import namedtuple
-import os 
+import os
 import inspect
-from seqcluster.preparedata import _read_fasta_files,_create_matrix_uniq_seq
+from seqcluster.preparedata import _read_fastq_files, _create_matrix_uniq_seq
 import seqcluster
 
 
 class TestPreparedata(TestCase):
     def test_preparedata(self):
         mod_dir = os.path.dirname(inspect.getfile(seqcluster)).replace("seqcluster/","")
-        os.chdir(os.path.join(mod_dir, "data"))
-        if os.path.exists("pre/seqs.ma"):
-            os.remove("pre/seqs.ma")
-        if os.path.exists("pre/seqs.fa"):
-            os.remove("pre/seqs.fa")
+        os.chdir(os.path.join(mod_dir, "data/test_collapse"))
+        if os.path.exists("seqs.ma"):
+            os.remove("seqs.ma")
+        if os.path.exists("seqs.fa"):
+            os.remove("seqs.fa")
         arg = namedtuple('args', 'minl maxl minc')
         args = arg(15, 40, 1)
-        seq_l, list_s = _read_fasta_files(open("config"), args)
-        ma_out = open("pre/seqs.ma", 'w')
-        seq_out = open("pre/seqs.fa", 'w')
+        seq_l, list_s = _read_fastq_files(open("config"), args)
+        ma_out = open("seqs.ma", 'w')
+        seq_out = open("seqs.fa", 'w')
         _create_matrix_uniq_seq(list_s, seq_l, ma_out, seq_out)
-        self.assertTrue(os.path.exists("pre/seqs.ma"))
-        self.assertTrue(os.path.exists("pre/seqs.fa"))
+        self.assertTrue(os.path.exists("seqs.ma"))
+        self.assertTrue(os.path.exists("seqs.fa"))
