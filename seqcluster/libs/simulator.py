a17d28e91a2bbd6f2b6c0cc5aa8c1d801148c40b
Lorena <lorena.pantano@gmail.com>
2015-03-05 17:23:54 -0500

    add template to simultor

---
+++ seqcluster/libs/simulator.py
@@ -0,0 +1,27 @@
+"""simulate cluster over the genome"""
+
+
+def _get_precursor(locus, reference):
+    """
+    get sequence from position
+    """
+    return 0
+
+def _get_spot(precursor):
+    """
+    get spot that will be enriched
+    """
+    return 0
+
+def _get_type():
+    """
+    randomly decide if is small rna or degradation
+    """
+    return 0
+
+def _random_sequences(precursor, start= None, end=None):
+    """
+    randomly get sequences around some nucleotides.
+    It could be enriched in some positions
+    """
+    return 0
