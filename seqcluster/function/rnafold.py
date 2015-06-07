fa37e8d64f6d709fa9264f0e636046b06a940125
Lorena Pantano <lorena.pantano@gmail.com>
2015-06-07 19:17:48 -0400

    name steps to create rnafold feature

--- seqcluster/function/rnafold.py
+++ seqcluster/function/rnafold.py
@@ -3,6 +3,9 @@ Wrap RNAfold command
 """
 
 import subprocess
+import pybedtools
+
+from bcbio.utils import splitext_plus
 
 
 def run_rnafold(seqs):
@@ -14,3 +17,9 @@ def run_rnafold(seqs):
         if line.find(" ") > -1:
             out = float(line.split(" ")[1][1:7])
     return out
+
+def calculate_structure(loci_file):
+    structure_file = splitext_plus(loci_file, "-fold.tsv")
+    loci_bed = pybedtools.BedTool(loci_file)
+    # getfasta with reference fasta
+    # for line: get fasta -> run_rnafold -> add 
