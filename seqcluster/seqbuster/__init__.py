b3936919293e61eab5b0bf74fa830e5d3521d2ea
Lorena Pantano <lorena.pantano@gmail.com>
2015-07-18 11:51:16 -0400

    isomirs annotation in realign bam file

--- seqcluster/seqbuster/__init__.py
+++ seqcluster/seqbuster/__init__.py
@@ -15,7 +15,7 @@ def _get_pos(string):
     return name, pos
 
 def _read_mature(matures, sps):
-    mature = {}
+    mature = defaultdict(dict)
     with open(matures) as in_handle:
         for line in in_handle:
             if line.startswith(">"):
@@ -23,9 +23,9 @@ def _read_mature(matures, sps):
                 mir5p = _get_pos(name[2])
                 if len(name) > 3:
                     mir3p = _get_pos(name[3])
+                mature[name[0]] = {mir5p[0]: map(int, mir5p[1]),
+                                   mir3p[0]: map(int, mir3p[1])}
     return mature
-
-
 
 def _read_precursor(precursor, sps):
     """
@@ -47,10 +47,45 @@ def _download_mirbase(precursor, reference, version="22"):
     if not precursors or not reference:
         print "Working with version %s" % version
 
-def _annotate(read, mirbase_ref):
+def _coord(sequence, start, mirna, precursor, iso):
+    """
+    Define t5 and t3 isomirs
+    """
+    dif = abs(mirna[0] - start)
+    if start < mirna[0]:
+        iso.t5 = "I" + sequence[:dif]
+    elif start > mirna[0]:
+        iso.t5 = "D" + precursor[mirna[0] - 1:mirna[0] - 1 + dif]
+
+    end = start + (len(sequence) - len(iso.add)) - 1
+    print "%s %s %s " % (start, len(sequence), end)
+    dif = abs(mirna[1] - end)
+    if dif > 3:
+        return None
+    if end > mirna[1]:
+        iso.t3 = "I" + sequence[-dif:]
+    elif end < mirna[1]:
+        iso.t3 = "D" + precursor[mirna[1] - dif:mirna[1]]
+
+def _annotate(reads, mirbase_ref, precursors):
     """
     Using SAM/BAM coordinates, mismatches and realign to annotate isomiRs
     """
+    for r in reads:
+        for p in reads[r].precursors:
+            # print r
+            # print reads[r].sequence
+            start = reads[r].precursors[p].start + 1 # convert to 1base
+            for mature in mirbase_ref[p]:
+                mi = mirbase_ref[p][mature]
+                if start < mi[0] + 4 and start > mi[0] - 4:
+                    # print precursors[p][start - 1:]
+                    # print precursors[p][mi[0]-1:mi[1]]
+                    # print mi
+                    _coord(reads[r].sequence, start, mi, precursors[p], reads[r].precursors[p])
+                    # print reads[r].precursors[p].format()
+                    break
+    return reads
 
 def _realign(seq, precursor, start):
     """
@@ -58,13 +93,52 @@ def _realign(seq, precursor, start):
     """
     # print seq
     # print precursor[start:]
+    error = set()
+    pattern_addition = [[1, 1, 0], [1, 0, 1], [0, 1, 0], [0, 1, 1], [0, 0, 1], [1, 1, 1]]
+    for pos in range(0, len(seq)):
+        if seq[pos] != precursor[(start + pos)]:
+            error.add(pos)
 
-def _realign_hits(read):
+    subs, add = [], []
+    for e in error:
+        if e < len(seq) - 3:
+            subs.append([e, precursor[start + e]])
+
+    pattern, error_add = [], []
+    for e in range(len(seq) - 3, len(seq)):
+        if e in error:
+            pattern.append(1)
+            error_add.append(e)
+        else:
+            pattern.append(0)
+    for p in pattern_addition:
+        if pattern == p:
+            add = seq[error_add[0]:]
+            break
+    if not add and error_add:
+        for e in error_add:
+            subs.append([e, precursor[start + e]])
+
+    return subs, add
+
+def _clean_hits(reads):
     """
-    realign read to the list precursors
+    Select only best matches
     """
-    for genome in read.precursors:
-        new_hit = _realign(read.sequence, genome)
+    new_reads = defaultdict(realign)
+    for r in reads:
+        world = {}
+        sc = 0
+        for p in reads[r].precursors:
+            world[p] = reads[r].precursors[p].get_score(len(reads[r].sequence))
+            if sc < world[p]:
+                sc = world[p]
+        new_reads[r] = reads[r]
+        for p in world:
+            if sc != world[p]:
+                new_reads[r].remove_precursor(p)
+
+    return new_reads
 
 def _sort_by_name(bam_fn):
     """
@@ -82,14 +156,20 @@ def _read_bam(bam_fn, precursors):
         # print "%s %s %s %s" % (line.query_name, line.reference_start, line.query_sequence, chrom)
         if line.query_name not in reads:
             reads[line.query_name].sequence = line.query_sequence
-        _realign(reads[line.query_name].sequence, precursors[chrom], line.reference_start)
+        iso = isomir()
+        iso.align = line
+        iso.start = line.reference_start
+        iso.subs, iso.add = _realign(reads[line.query_name].sequence, precursors[chrom], line.reference_start)
+        reads[line.query_name].set_precursor(chrom, iso)
 
+    reads = _clean_hits(reads)
+    return reads
 
 def miraligner(args):
     """
     Realign BAM hits to miRBAse to get better accuracy and annotation
     """
-    config = {"algorithm": {"num_cores":1}}
+    config = {"algorithm": {"num_cores": 1}}
     precursors = _read_precursor(args.hairpin, args.sps)
     matures = _read_mature(args.mirna, args.sps)
     for bam_fn in args.files:
@@ -98,3 +178,4 @@ def miraligner(args):
         bam_sort_by_n = op.splitext(bam_fn)[0] + "_sort"
         pysam.sort("-n", bam_fn, bam_sort_by_n)
         reads = _read_bam(bam_sort_by_n + ".bam", precursors)
+        _annotate(reads, matures, precursors)
--- seqcluster/seqbuster/realign.py
+++ seqcluster/seqbuster/realign.py
@@ -4,17 +4,36 @@ class realign:
 
     def __init__(self):
         self.sequence = ""
-        self.precursor = defaultdict(isomir)
+        self.precursors = defaultdict(isomir)
         self.score = []
         self.best_hits = [] # maybe sam object?
 
     def set_precursor(self, precursor, isomir):
-        self.precursor[precursor] = isomir
+        self.precursors[precursor] = isomir
+
+    def remove_precursor(self, precursor):
+        del self.precursors[precursor]
 
 class isomir:
 
     def __init__(self):
-        self.t5 = ""
-        self.t3 = ""
-        self.add = ""
-        self.subs = ""
+        self.t5 = 0
+        self.t3 = 0
+        self.add = []
+        self.subs = []
+        self.align = None
+        self.start = 0
+
+    def format(self):
+        return "subs %s add %s %s %s" % (self.subs, self.add,
+                                         self.t5, self.t3)
+
+    def get_score(self, sc):
+        for a in self.add:
+            if a in ['A', 'T']:
+                sc -= 0.25
+            else:
+                sc -= 0.75
+        for e in self.subs:
+            sc -= 1
+        return sc
