d24bd49e5e2161e4d994d2ebfa47873de4fd2c8d
Lorena <lorena.pantano@gmail.com>
2015-08-11 13:20:11 -0400

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as f:
 
 
 setup(name='seqcluster',
-      version='1.1.7',
+      version='1.1.8',
       description='Small RNA-seq pipeline',
       long_description=readme(),
       classifiers=[
