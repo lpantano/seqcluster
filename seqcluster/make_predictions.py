30b395f5538461638d7702393fa07970b9421e35
Lorena <lorena.pantano@gmail.com>
2015-08-30 10:31:51 -0400

    write new json object

--- seqcluster/make_predictions.py
+++ seqcluster/make_predictions.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from libs.read import load_data
+from libs.read import load_data, write_data
 from function.predictions import is_tRNA, run_coral
 from libs.utils import safe_dirs
 
@@ -15,14 +15,15 @@ def predictions(args):
 
     logger.info(args)
     logger.info("reading sequeces")
+    out_file = os.path.abspath(os.path.splitext(args.json)[0] + "_prediction.json")
     data = load_data(args.json)
     out_dir = os.path.abspath(safe_dirs(os.path.join(args.out, "predictions")))
 
     logger.info("make predictions")
-    is_tRNA(data, out_dir, args)
+    data = is_tRNA(data, out_dir, args)
 
     if args.coral:
         logger.info("make CoRaL predictions")
         run_coral(data, out_dir, args)
-
+    write_data(data[0], out_file)
     logger.info("Done")
