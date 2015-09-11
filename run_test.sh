2a588ac45a5e3f1bb1d6ce7c9d87ff15d9c559f6
Lorena <lorena.pantano@gmail.com>
2015-09-11 09:00:25 -0400

    Add info to testing script

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+# bash script inspared from bcbio-nextgen: https://github.com/chapmanb/bcbio-nextgen/blob/master/tests/run_tests.sh
+# It allows to run specific test using the name like
+# ./run_test.sh test_align
+set -e
+
 readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' $1; }
 SC_DIR=$(dirname "$(readlinkf `which seqcluster`)")
 unset PYTHONHOME
