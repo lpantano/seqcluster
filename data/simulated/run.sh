53df4bdd101976bb0a2760e24b9ca33f78b3cf75
Lorena <lorena.pantano@gmail.com>
2015-09-19 19:46:11 -0400

    Add simulation of 2 tRNAs precursors
    With this we can improve the algorithm to
    get a better specificity and quantification

---
+++ data/simulated/run.sh
@@ -0,0 +1,9 @@
+seqcluster simulator --fasta origin.fa --out sim
+bowtie -f -a --best --strata ~/soft/bcbio/genomes/Hsapiens/hg19/bowtie/hg19 sim.fasta -S sim.sam
+samtools view -Sbh sim.sam > sim.bam
+samtools sort  sim.bam sim_sort
+
+rm -rf res sim_sort_*
+seqcluster cluster -a sim_sort.bam -m sim.ma -o res -r ~/soft/bcbio/genomes/Hsapiens/hg19/seq/hg19.fa
+#seqcluster predict -j res/seqcluster.json -o predict --reference  ~/orch/groups/bcbio/genomes/Hsapiens/hg19/seq/hg19.fa
+#seqcluster report -j res/seqcluster.json -o report -r  ~/orch/groups/bcbio/genomes/Hsapiens/hg19/seq/hg19.fa
