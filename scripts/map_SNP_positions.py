bffe5d21101d3bb8684ed20835dd5483d0cebfea
Lorena <lorena.pantano@gmail.com>
2015-11-08 19:05:37 -0500

    Functional script to create known SNPs with miRNA reference.

--- scripts/map_SNP_positions.py
+++ scripts/map_SNP_positions.py
@@ -19,25 +19,50 @@ def _get_mirna_name(attrs):
     items = attrs.split(";")
     return [i.split("=")[1] for i in items if i.startswith("Name")][0]
 
-def select_snps(mirna, snp):
+def _create_header(mirna, snp, out):
+    names = set()
+    h = []
+    last_h = ""
+    with open(snp) as in_handle:
+        for line in in_handle:
+            if line.startswith("##") and not line.startswith("##contig"):
+                h.append(line.strip())
+            elif line.startswith("#CHROM"):
+                last_h = line.strip()
+            else:
+                break
+    with open(mirna) as in_handle:
+        for line in in_handle:
+            cols = line.strip().split("\t")
+            names.add(_get_mirna_name(cols[8]))
+        for name in names:
+            h.append("##contig=<ID=%s>" % name)
+    h.append(last_h)
+    return "\n".join(h)
+
+def select_snps(mirna, snp, out):
     """
     Use bedtools to intersect coordinates
     """
-    snp_in_mirna = pybedtools.BedTool(mirna).intersect(pybedtools.BedTool(snp), wo=True)
-    for single in snp_in_mirna:
-        if single[2] == "miRNA":
-            rel_p = _lift_positions(single)
-            single[10] = str(rel_p)
-            single[12] = _complement(single[12], single[6])
-            single[13] = _complement(single[13], single[6])
-            single[9] = _get_mirna_name(single[8])
-            print single[9:]
+    with open(out, 'w') as out_handle:
+        print >>out_handle, _create_header(mirna, snp, out)
+        snp_in_mirna = pybedtools.BedTool(mirna).intersect(pybedtools.BedTool(snp), wo=True)
+        for single in snp_in_mirna:
+            if single[2] == "miRNA" and len(single[12]) + len(single[13]) == 2:
+                rel_p = _lift_positions(single)
+                single[10] = str(rel_p)
+                single[12] = _complement(single[12], single[6])
+                single[13] = _complement(single[13], single[6])
+                single[9] = _get_mirna_name(single[8])
+                print >>out_handle, "\t".join(single[9:])
+    return out
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="task related to allele methylation specific")
     parser.add_argument("--gtf", help="gtf file with miRNA annotation", required=1)
     parser.add_argument("--vcf", help="vcf file with SNP variants", required=1)
+    parser.add_argument("--out", help="out put file.", required=1)
     args = parser.parse_args()
 
-    mirna_snp = select_snps(args.gtf, args.vcf)
+    mirna_snp = select_snps(args.gtf, args.vcf, args.out)
     print "%s mapped to %s: %s" % (args.vcf, args.gtf, mirna_snp)
