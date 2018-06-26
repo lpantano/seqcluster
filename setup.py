9ea657a704fc33f0811e3fad3a365d46a9c95ddf
Lorena Pantano <lorena.pantano@gmail.com>
2018-06-26 16:19:28 -0700

    fix version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='seqcluster',
-      version='1.2.5a3',
+      version='1.2.4a4',
       description='Small RNA-seq pipeline',
       long_description=readme(),
       classifiers=[
